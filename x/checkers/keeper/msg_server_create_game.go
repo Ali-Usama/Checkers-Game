6f258499c3bbc0ab75b835ebe9aa711d857ab8b0
Usama Ali <aliusama481@yahoo.com>
2023-03-20 10:31:02 +0500

    [ADD] * message handler to create/save a game

--- x/checkers/keeper/msg_server_create_game.go
+++ x/checkers/keeper/msg_server_create_game.go
@@ -2,7 +2,9 @@ package keeper
 
 import (
 	"context"
+	"strconv"
 
+	"github.com/alice/checkers/x/checkers/rules"
 	"github.com/alice/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -10,8 +12,38 @@ import (
 func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (*types.MsgCreateGameResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// Get the new game's ID
+	systemInfo, found := k.Keeper.GetSystemInfo(ctx)
+	if !found {
+		panic("SystemInfo not found")
+	}
+	newIndex := strconv.FormatUint(systemInfo.NextId, 10)
 
-	return &types.MsgCreateGameResponse{}, nil
+	// Create the object to be stored
+	newGame := rules.New()
+	storedGame := types.StoredGame{
+		Index: newIndex,
+		Board: newGame.String(),
+		Turn:  rules.PieceStrings[newGame.Turn],
+		Black: msg.Black,
+		Red:   msg.Red,
+	}
+
+	// Confirm that the values in the object are correct by checking the validity of the players' addresses
+	err := storedGame.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	// Save the `storedGame` object
+	k.Keeper.SetStoredGame(ctx, storedGame)
+
+	// prepare the ground for the next game
+	systemInfo.NextId++
+	k.Keeper.SetSystemInfo(ctx, systemInfo)
+
+	// return the newly created ID for reference
+	return &types.MsgCreateGameResponse{
+		GameIndex: newIndex,
+	}, nil
 }
