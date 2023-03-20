6c3ecc23a5f824bcfaaa8f5c61ea2dcf36d49237
Usama Ali <aliusama481@yahoo.com>
2023-03-20 14:23:58 +0500

    [ADD] * move handling

--- x/checkers/keeper/msg_server_play_move.go
+++ x/checkers/keeper/msg_server_play_move.go
@@ -2,6 +2,8 @@ package keeper
 
 import (
 	"context"
+	"github.com/alice/checkers/x/checkers/rules"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	"github.com/alice/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -10,8 +12,61 @@ import (
 func (k msgServer) PlayMove(goCtx context.Context, msg *types.MsgPlayMove) (*types.MsgPlayMoveResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// fetch the storedGame information
+	storedGame, found := k.Keeper.GetStoredGame(ctx, msg.GameIndex)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrGameNotFound, "%s", msg.GameIndex)
+	}
 
-	return &types.MsgPlayMoveResponse{}, nil
+	// is the player legitimate?
+	isBlack := storedGame.Black == msg.Creator
+	isRed := storedGame.Red == msg.Creator
+	var player rules.Player
+
+	if !isBlack && !isRed {
+		return nil, sdkerrors.Wrapf(types.ErrCreatorNotPlayer, "%s", msg.Creator)
+	} else if isBlack && isRed {
+		player = rules.StringPieces[storedGame.Turn].Player
+	} else if isBlack {
+		player = rules.BLACK_PLAYER
+	} else {
+		player = rules.RED_PLAYER
+	}
+
+	// instantiate the board in order to implement the rules
+	game, err := storedGame.ParseGame()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// is it the player's turn?
+	if !game.TurnIs(player) {
+		return nil, sdkerrors.Wrapf(types.ErrNotPlayerTurn, "%s", player)
+	}
+
+	// properly conduct the move
+	captured, moveErr := game.Move(
+		rules.Pos{
+			X: int(msg.FromX),
+			Y: int(msg.FromY),
+		},
+		rules.Pos{
+			X: int(msg.ToX),
+			Y: int(msg.ToY),
+		},
+	)
+	if moveErr != nil {
+		return nil, sdkerrors.Wrapf(types.ErrWrongMove, moveErr.Error())
+	}
+
+	// prepare the updated board to be stored and store the information
+	storedGame.Board = game.String()
+	storedGame.Turn = rules.PieceStrings[game.Turn]
+	k.Keeper.SetStoredGame(ctx, storedGame)
+
+	return &types.MsgPlayMoveResponse{
+		CapturedX: int32(captured.X),
+		CapturedY: int32(captured.Y),
+		Winner:    rules.PieceStrings[game.Winner()],
+	}, nil
 }
