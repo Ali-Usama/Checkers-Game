21219aac9ea6ddc507e76999e7198c14a12990d5
Usama Ali <aliusama481@yahoo.com>
2023-03-22 10:12:38 +0500

    [ADD] * Auto-Expiring Game

--- vue/src/store/generated/alice.checkers.checkers/index.ts
+++ vue/src/store/generated/alice.checkers.checkers/index.ts
@@ -223,16 +223,16 @@ export default {
 		},
 		
 		
-		async sendMsgPlayMove({ rootGetters }, { value, fee = [], memo = '' }) {
+		async sendMsgRejectGame({ rootGetters }, { value, fee = [], memo = '' }) {
 			try {
 				const client=await initClient(rootGetters)
-				const result = await client.AliceCheckersCheckers.tx.sendMsgPlayMove({ value, fee: {amount: fee, gas: "200000"}, memo })
+				const result = await client.AliceCheckersCheckers.tx.sendMsgRejectGame({ value, fee: {amount: fee, gas: "200000"}, memo })
 				return result
 			} catch (e) {
 				if (e == MissingWalletError) {
-					throw new Error('TxClient:MsgPlayMove:Init Could not initialize signing client. Wallet is required.')
+					throw new Error('TxClient:MsgRejectGame:Init Could not initialize signing client. Wallet is required.')
 				}else{
-					throw new Error('TxClient:MsgPlayMove:Send Could not broadcast Tx: '+ e.message)
+					throw new Error('TxClient:MsgRejectGame:Send Could not broadcast Tx: '+ e.message)
 				}
 			}
 		},
@@ -249,30 +249,30 @@ export default {
 				}
 			}
 		},
-		async sendMsgRejectGame({ rootGetters }, { value, fee = [], memo = '' }) {
+		async sendMsgPlayMove({ rootGetters }, { value, fee = [], memo = '' }) {
 			try {
 				const client=await initClient(rootGetters)
-				const result = await client.AliceCheckersCheckers.tx.sendMsgRejectGame({ value, fee: {amount: fee, gas: "200000"}, memo })
+				const result = await client.AliceCheckersCheckers.tx.sendMsgPlayMove({ value, fee: {amount: fee, gas: "200000"}, memo })
 				return result
 			} catch (e) {
 				if (e == MissingWalletError) {
-					throw new Error('TxClient:MsgRejectGame:Init Could not initialize signing client. Wallet is required.')
+					throw new Error('TxClient:MsgPlayMove:Init Could not initialize signing client. Wallet is required.')
 				}else{
-					throw new Error('TxClient:MsgRejectGame:Send Could not broadcast Tx: '+ e.message)
+					throw new Error('TxClient:MsgPlayMove:Send Could not broadcast Tx: '+ e.message)
 				}
 			}
 		},
 		
-		async MsgPlayMove({ rootGetters }, { value }) {
+		async MsgRejectGame({ rootGetters }, { value }) {
 			try {
 				const client=initClient(rootGetters)
-				const msg = await client.AliceCheckersCheckers.tx.msgPlayMove({value})
+				const msg = await client.AliceCheckersCheckers.tx.msgRejectGame({value})
 				return msg
 			} catch (e) {
 				if (e == MissingWalletError) {
-					throw new Error('TxClient:MsgPlayMove:Init Could not initialize signing client. Wallet is required.')
+					throw new Error('TxClient:MsgRejectGame:Init Could not initialize signing client. Wallet is required.')
 				} else{
-					throw new Error('TxClient:MsgPlayMove:Create Could not create message: ' + e.message)
+					throw new Error('TxClient:MsgRejectGame:Create Could not create message: ' + e.message)
 				}
 			}
 		},
@@ -289,16 +289,16 @@ export default {
 				}
 			}
 		},
-		async MsgRejectGame({ rootGetters }, { value }) {
+		async MsgPlayMove({ rootGetters }, { value }) {
 			try {
 				const client=initClient(rootGetters)
-				const msg = await client.AliceCheckersCheckers.tx.msgRejectGame({value})
+				const msg = await client.AliceCheckersCheckers.tx.msgPlayMove({value})
 				return msg
 			} catch (e) {
 				if (e == MissingWalletError) {
-					throw new Error('TxClient:MsgRejectGame:Init Could not initialize signing client. Wallet is required.')
+					throw new Error('TxClient:MsgPlayMove:Init Could not initialize signing client. Wallet is required.')
 				} else{
-					throw new Error('TxClient:MsgRejectGame:Create Could not create message: ' + e.message)
+					throw new Error('TxClient:MsgPlayMove:Create Could not create message: ' + e.message)
 				}
 			}
 		},
